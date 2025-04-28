00a123bfdb1c2ae39393129672bbcd176f2d604a
Benjamin <Benjamin@gmail.com>
2025-04-28 21:41:41 +0700

    feature/F105: implement pokemon controller

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Api.Dtos;
+using PokemonReviewApp.Api.Entities;
+using PokemonReviewApp.Api.Repositories;
+using PokemonReviewApp.Api.Services;
+
+namespace PokemonReviewApp.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PokemonController : Controller
+{
+    
+    private readonly IPokemonService _service;
+    private readonly IMapper _mapper;
+
+    public PokemonController(IPokemonService service, IMapper mapper)
+    {
+        _service = service;
+        _mapper = mapper;
+    }
+    
+    // GET
+    [HttpGet]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+    public IActionResult GetPokemonList()
+    {
+        var pokemonList = _mapper.Map<PokemonDto>(_service.GetAll());
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        return Ok(pokemonList);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(200, Type = typeof(Pokemon))]
+    [ProducesResponseType(404)]
+    public IActionResult GetPokemon(int id)
+    {
+        if (!_service.PokemonExists(id))
+        {
+            return NotFound();
+        }
+        var pokemon = _mapper.Map<PokemonDto>(_service.GetById(id));
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+        return Ok(pokemon);
+    }
+
+    [HttpGet("{id}/ratings")]
+    [ProducesResponseType(200, Type = typeof(decimal))]
+    [ProducesResponseType(404)]
+    public IActionResult GetPokemonRating(int id)
+    {
+        if (!_service.PokemonExists(id))
+        {
+            return NotFound();
+        }
+        var rating = _service.GetRating(id);
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        return Ok(rating);
+    }
+    
+    
+}
---
+++ Services/PokemonService.cs
@@ -0,0 +1,46 @@
+﻿using PokemonReviewApp.Api.Data;
+using PokemonReviewApp.Api.Entities;
+using PokemonReviewApp.Api.Repositories;
+
+namespace PokemonReviewApp.Api.Services;
+
+public class PokemonService : IPokemonService
+{
+
+    private readonly DataContext _context;
+
+    public PokemonService(DataContext context)
+    {
+        _context = context;
+    }
+    
+    public ICollection<Pokemon> GetAll()
+    {
+        return _context.Pokemon.OrderBy(p => p.Name).ToList();
+    }
+
+    public Pokemon GetById(int id)
+    {
+        return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+    }
+
+    public Pokemon GetByName(string name)
+    {
+        return _context.Pokemon.Where(p => p.Name == name).FirstOrDefault();
+    }
+
+    public decimal GetRating(int id)
+    {
+        var review = _context.Reviews.Where(p => p.Pokemon.Id == id);
+        if (review.Count() <= 0)
+        {
+            return 0;
+        }
+        return ((decimal)review.Sum(r => r.Rating) / review.Count());
+    }
+
+    public bool PokemonExists(int id)
+    {
+        return _context.Pokemon.Any(p => p.Id == id);
+    }
+}
