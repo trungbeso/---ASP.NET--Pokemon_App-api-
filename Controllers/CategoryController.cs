de92dd3bc4be8f423aa0ed43c78f947a99c00c59
Benjamin <Benjamin@gmail.com>
2025-04-28 22:06:29 +0700

    feature/H201: create Category controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Api.Dtos;
+using PokemonReviewApp.Api.Entities;
+using PokemonReviewApp.Api.Repositories;
+
+namespace PokemonReviewApp.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoryController : Controller
+{
+    private readonly ICategoryService _service;
+    private readonly IMapper _mapper;
+
+    public CategoryController(ICategoryService categoryService, IMapper mapper)
+    {
+        _service = categoryService;
+        _mapper = mapper;
+    }
+    
+    [HttpGet]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
+    public IActionResult GetCategories()
+    {
+        var categories = _mapper.Map<CategoiryDto>(_service.GetAll());
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        return Ok(categories);
+    }
+    
+    [HttpGet("{categoryId}")]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
+    [ProducesResponseType(404)]
+    public IActionResult GetCategoryById(int categoryId)
+    {
+        if (!_service.CategoryExists(categoryId))
+        {
+            return NotFound();
+        }
+        var category = _mapper.Map<CategoiryDto>(_service.GetById(categoryId));
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+        return Ok(category);
+    }
+
+    [HttpGet("pokemon/{categoryId}")]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
+    [ProducesResponseType(404)]
+    public IActionResult GetPokemonByCategoryId(int categoryId)
+    {
+        var pokemons = _mapper.Map<List<PokemonDto>>(_service.GetPokemonByCategory(categoryId));
+
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        return Ok(pokemons);
+    }
+    
+    
+}
---
+++ Repositories/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using PokemonReviewApp.Api.Entities;
+
+namespace PokemonReviewApp.Api.Repositories;
+
+public interface ICategoryService
+{
+    ICollection<Category> GetAll();
+    Category GetById(int id);
+    ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+    bool CategoryExists(int categoryId);
+}
